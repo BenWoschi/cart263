55ce7373a7a129006de335ab978d7c3b64460f74
BenWoschi <benjaminw4101@gmail.com>
2026-01-15 17:12:10 -0500

    Continue question 4
    Continue question 4

--- exercises/task1/questions/question-4/js/script.js
+++ exercises/task1/questions/question-4/js/script.js
@@ -6,40 +6,35 @@
 "use strict";
 
 let rectangle1 = {
-    x: 50,
-    y: 50,
-    size: 75,
-    
-    fill: {
-        r: 227,
-        g: 27,
-        b: 74,
-    }
-};
-
-let rectangle2 = {
-    x: 150,
-    y: 150,
-    size: 90,
+    x: 0,
+    y: 0,
     
     fill: {
         r: 27,
         g: 227,
-        b: 67,
+        b: 184,
     }
 };
 
-let rectangle3 = {
-    x: 290,
-    y: 25,
-    size: 40,
-    
+let rectangle2 = {
+
     fill: {
         r: 27,
         g: 210,
         b: 227,
     }
 };
+
+let rectangle3 = {
+
+    fill: {
+        r: 27,
+        g: 67,
+        b: 227,
+    }
+};
+
+
 
 function setup() {
     createCanvas(400, 400);
@@ -48,46 +43,25 @@ function setup() {
 
 function draw() {
     background("#2b2b2b");
-    clickRectangle();
-    pressRectangle();
-    movingRectangle();
+    rect1();
+    rect2();
+    rect3();
 }
 
-function clickRectangle() {
+function rect1() {
     noStroke();
     fill(rectangle1.fill.r, rectangle1.fill.g, rectangle1.fill.b);
-    rect(rectangle1.x, rectangle1.y, rectangle1.size);
+    rect(rectangle1.x, rectangle1.y, width / 3, height);
 }
 
-function mousePressed() {
-    if (mousePressed) {
-        rectangle1.x = mouseX;
-        rectangle1.y = mouseY;
-    }
-}
-
-function pressRectangle() {
+function rect2() {
     noStroke();
     fill(rectangle2.fill.r, rectangle2.fill.g, rectangle2.fill.b);
-    rect(rectangle2.x, rectangle2.y, rectangle2.size);
+    rect(width / 3, 0, width / 3, height);
 }
 
-function keyPressed() {
-    if (key === ' ') {
-        rectangle2.x = mouseX;
-        rectangle2.y = mouseY;
-    }
-}
-
-function movingRectangle() {
+function rect3() {
     noStroke();
     fill(rectangle3.fill.r, rectangle3.fill.g, rectangle3.fill.b);
-    rect(rectangle3.x, rectangle3.y, rectangle3.size);
-
-    if (rectangle3.y >= 400) {
-        rectangle3.y = 0;
-    }
-    else {
-        rectangle3.y += 3;
-    }
+    rect(width / 3 * 2, 0, width / 3, height);
 }
